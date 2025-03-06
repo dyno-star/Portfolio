6c24c0d0b1083fc987f1aa09cc8ef408b888a992
dyno-star <da893@drexel.edu>
2025-03-06 15:41:17 -0500

    Grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -7,9 +7,15 @@ const Grid = () => {
     <section id='about'>
         <BentoGrid>
             {[
-                { title: "Title1", description: 'Desc1' }
-            ].map((item, index) => (
-                <BentoGridItem key={index} />
+                { title: "Title1", description: 'Desc1',
+                  id : 1
+                 }].map
+            ((item, i) => (
+                <BentoGridItem
+                id={item.id} 
+                key={item.id}
+                title={item.title} 
+                description={item.description}/>
             ))}
         </BentoGrid>
     </section>
